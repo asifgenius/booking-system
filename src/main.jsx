f96091053fa034bc4c4e4f658e7a48f65b8cec6d
Asif Islam <engrasifislam@gmail.com>
2026-03-17 14:14:04 +0600

    Integrate ToastContainer in main file

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
+import App from './App';
+import 'react-toastify/dist/ReactToastify.css';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <ToastContainer
+      position="top-right"
+      autoClose={2400}
+      hideProgressBar={false}
+      newestOnTop
+      closeOnClick
+      pauseOnHover
+      theme="colored"
+    />
+  </React.StrictMode>,
+);
